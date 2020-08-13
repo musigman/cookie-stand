690de39ff4c5ffef92292284405062a4a402c6b4
Keith Musig <musigman@gmail.com>
2020-08-12 20:00:38 -0700

    Lab 09

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 var seattle = {
   minPeople: 23,
@@ -11,16 +11,18 @@ var seattle = {
       var seattleCustomers = Math.random() * (this.maxPeople - this.minPeople) + this.minPeople;
       var cookieNumber = seattleCustomers * this.avgCookie;
       var cookiesBaked = Math.ceil(cookieNumber);
-      this.cookieDayTotal = this.cookieDayTotal + cookiesBaked;  // this.cookieDayTotal += CookiesBaked
+      this.cookieDayTotal = this.cookieDayTotal + cookiesBaked; // this.cookieDayTotal += CookiesBaked
       var parent = document.getElementById('seattle');
       var listItem = document.createElement('li');
       listItem.textContent = `at ${time[i]} make ${cookiesBaked} cookies`;
       parent.appendChild(listItem);
     }
-  document.getElementById('seattleTotal').innerHTML = `Bake ${this.cookieDayTotal} cookies in total for the whole day.`;
+    document.getElementById('seattleTotal').innerHTML = `Bake ${this.cookieDayTotal} cookies in total for the whole day.`;
   }
-}
+};
 seattle.render();
+
+
 var tokyo = {
   minPeople: 3,
   maxPeople: 24,
@@ -32,7 +34,7 @@ var tokyo = {
       var tokyoCustomers = Math.random() * (this.maxPeople - this.minPeople) + this.minPeople;
       var cookieNumber = tokyoCustomers * this.avgCookie;
       var cookiesBaked = Math.ceil(cookieNumber);
-      this.cookieDayTotal = this.cookieDayTotal + cookiesBaked;  // this.cookieDayTotal += CookiesBaked
+      this.cookieDayTotal = this.cookieDayTotal + cookiesBaked; // this.cookieDayTotal += CookiesBaked
       var parent = document.getElementById('tokyo');
       var listItem = document.createElement('li');
       listItem.textContent = `at ${time[i]} make ${cookiesBaked} cookies`;
@@ -40,7 +42,7 @@ var tokyo = {
     }
     document.getElementById('tokyoTotal').innerHTML = `Bake ${this.cookieDayTotal} cookies in total for the whole day.`;
   }
-}
+};
 tokyo.render();
 var dubai = {
   minPeople: 11,
@@ -53,7 +55,7 @@ var dubai = {
       var dubaiCustomers = Math.random() * (this.maxPeople - this.minPeople) + this.minPeople;
       var cookieNumber = dubaiCustomers * this.avgCookie;
       var cookiesBaked = Math.ceil(cookieNumber);
-      this.cookieDayTotal = this.cookieDayTotal + cookiesBaked;  // this.cookieDayTotal += CookiesBaked
+      this.cookieDayTotal = this.cookieDayTotal + cookiesBaked; // this.cookieDayTotal += CookiesBaked
       var parent = document.getElementById('dubai');
       var listItem = document.createElement('li');
       listItem.textContent = `at ${time[i]} make ${cookiesBaked} cookies`;
@@ -61,7 +63,7 @@ var dubai = {
     }
     document.getElementById('dubaiTotal').innerHTML = `Bake ${this.cookieDayTotal} cookies in total for the whole day.`;
   }
-}
+};
 dubai.render();
 var paris = {
   minPeople: 20,
@@ -74,15 +76,15 @@ var paris = {
       var parisCustomers = Math.random() * (this.maxPeople - this.minPeople) + this.minPeople;
       var cookieNumber = parisCustomers * this.avgCookie;
       var cookiesBaked = Math.ceil(cookieNumber);
-      this.cookieDayTotal = this.cookieDayTotal + cookiesBaked;  // this.cookieDayTotal += CookiesBaked
+      this.cookieDayTotal = this.cookieDayTotal + cookiesBaked; // this.cookieDayTotal += CookiesBaked
       var parent = document.getElementById('paris');
       var listItem = document.createElement('li');
       listItem.textContent = `at ${time[i]} make ${cookiesBaked} cookies`;
       parent.appendChild(listItem);
     }
- document.getElementById('parisTotal').innerHTML = `Bake ${this.cookieDayTotal} cookies in total for the whole day.`;
+    document.getElementById('parisTotal').innerHTML = `Bake ${this.cookieDayTotal} cookies in total for the whole day.`;
   }
-}
+};
 paris.render();
 var lima = {
   minPeople: 2,
@@ -95,13 +97,13 @@ var lima = {
       var limaCustomers = Math.random() * (this.maxPeople - this.minPeople) + this.minPeople;
       var cookieNumber = limaCustomers * this.avgCookie;
       var cookiesBaked = Math.ceil(cookieNumber);
-      this.cookieDayTotal = this.cookieDayTotal + cookiesBaked;  // this.cookieDayTotal += CookiesBaked
+      this.cookieDayTotal = this.cookieDayTotal + cookiesBaked; // this.cookieDayTotal += CookiesBaked
       var parent = document.getElementById('lima');
       var listItem = document.createElement('li');
       listItem.textContent = `at ${time[i]} make ${cookiesBaked} cookies`;
       parent.appendChild(listItem);
     }
-  document.getElementById('limaTotal').innerHTML = `Bake ${this.cookieDayTotal} cookies in total for the whole day.`;
+    document.getElementById('limaTotal').innerHTML = `Bake ${this.cookieDayTotal} cookies in total for the whole day.`;
   }
-}
+};
 lima.render();
